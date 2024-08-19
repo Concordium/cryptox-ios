7e11e7eaf873beafb3ada59b0e046bbd9bbf7dec
ZhannaKom <zk@concordium.com>
2024-08-19 18:41:14 +0300

    Added two-steps token import logic

--- ConcordiumWallet/Features/CIS2TokenSupport/ImportToken/ImportTokenView.swift
+++ ConcordiumWallet/Features/CIS2TokenSupport/ImportToken/ImportTokenView.swift
@@ -12,163 +12,138 @@ enum ImportTokenError {
     case tokeSaveFailed
 }
 
-@MainActor
-final class ImportTokenViewModel: ObservableObject {
-    @Published var tokens: [CIS2Token] = []
-    @Published var selectedToken: CIS2Token?
-    @Published var error: ImportTokenError?
-    @Published var isLoading: Bool = false
-    @Published var hasMore: Bool = true
-    @Published var currentPage = 1
-    
-    private let storageManager: StorageManagerProtocol
-    private let address: String
-    private var allContractTokens = [String]()
-    private let batchSize = 20
-    private var contractIndex: Int?
-    
-    private let cis2Service: CIS2Service
-    
-    init(storageManager: StorageManagerProtocol, networkManager: NetworkManagerProtocol, address: String) {
-        self.storageManager = storageManager
-        self.address = address
-        self.cis2Service = CIS2Service(networkManager: networkManager, storageManager: storageManager)
-        
-        logger.debugLog("savedTokens: -- \(self.storageManager.getAccountSavedCIS2Tokens(address))")
-    }
-    
-    func search(name: String) async {
-        do {
-            guard let index = Int(name) else { return }
-            allContractTokens = try await cis2Service.fetchTokens(contractIndex: name).tokens.map(\.token)
-            contractIndex = index
-            loadMore()
-        } catch {
-            logger.errorLog(error.localizedDescription)
-        }
-    }
-    
-    func saveToken(_ token: CIS2Token?) {
-        guard let token = token else { return }
-        guard !storageManager.getAccountSavedCIS2Tokens(address).contains(token) else { return }
-        
-        do {
-            try storageManager.storeCIS2Token(token: token, address: address)
-        } catch {
-            logger.errorLog(error.localizedDescription)
-        }
-    }
-    
-    func loadMore() {
-        guard !isLoading, hasMore, let contractIndex else { return }
-        
-        isLoading = true
-        
-        Task {
-            do {
-                let ids = allContractTokens.dropFirst((currentPage - 1) * batchSize).prefix(batchSize)
-                
-                guard !ids.isEmpty else {
-                    return await MainActor.run {
-                        hasMore = false
-                        isLoading = false
-                    }
-                }
-                
-                let fetchedTokens = try await self.cis2Service.fetchAllTokensData(contractIndex: contractIndex, tokenIds: ids.joined(separator: ","))
-                
-                await MainActor.run {
-                    
-                    if currentPage == 1 {
-                        tokens = fetchedTokens
-                    } else {
-                        tokens += fetchedTokens
-                    }
-                    hasMore = tokens.count < allContractTokens.count
-                    currentPage += 1
-                    isLoading = false
-                }
-            } catch {
-                await MainActor.run {
-                    isLoading = false
-                }
-            }
-        }
-    }
-}
-
 struct ImportTokenView: View {
     @StateObject var viewModel: ImportTokenViewModel
-    
     @State private var searchText: String = ""
+    @State private var searchTokenIdText: String = ""
+    @State private var showingTokenIdView = false
     @SwiftUI.Environment(\.dismiss) var dismiss
     
     var body: some View {
         NavigationView {
             VStack {
-                List(viewModel.tokens, id: \.tokenId) { token in
-                    Button {
-                        viewModel.selectedToken = token
-                    } label: {
-                        TokenView(token: token, isSelected: viewModel.selectedToken == token)
-                    }
-                    .onAppear {
-                        if token == viewModel.tokens.last {
-                            viewModel.loadMore()
-                        }
-                    }
-                    .listRowSeparator(.hidden)
+                if showingTokenIdView {
+                    searchTokenByTokenIDView()
+                } else {
+                    searchTokensByContractIndexView()
                 }
-                .listStyle(.plain)
+            }
+            .navigationTitle("nft.import.title")
+            .animation(.easeInOut, value: showingTokenIdView)
+        }
+    }
+    
+    func searchTokensByContractIndexView() -> some View {
+        VStack {
+            TextField("", text: $searchText)
                 .searchable(text: $searchText)
                 .keyboardType(.numberPad)
                 .onChange(of: searchText) { value in
                     Task {
-                        if !value.isEmpty &&  value.count > 3 {
+                        if !value.isEmpty && value.count > 3 {
                             await viewModel.search(name: value)
                         } else {
-                            viewModel.tokens.removeAll()
-                            viewModel.selectedToken = nil
+                            viewModel.initialSearchState()
                         }
                     }
                 }
-                .padding(.top, 16)
-                
-                HStack(spacing: 20) {
-                    Button {
-                        dismiss()
-                    } label: {
-                        Text("Cancel")
-                            .frame(maxWidth: .infinity)
-                            .foregroundColor(.white)
-                            .font(.system(size: 17, weight: .semibold))
-                            .padding(.vertical, 11)
-                            .background(Color.clear)
-                            .overlay(
-                                Capsule(style: .circular)
-                                    .stroke(.white, lineWidth: 2)
-                            )
-                    }
-                    
-                    Button {
-                        viewModel.saveToken(viewModel.selectedToken)
-                        dismiss()
-                    } label: {
-                        Text("Import")
-                            .frame(maxWidth: .infinity)
-                            .foregroundColor(.black)
-                            .font(.system(size: 17, weight: .semibold))
-                            .padding(.vertical, 11)
-                            .background(viewModel.selectedToken == nil ? .white.opacity(0.7) : .white)
-                            .clipShape(Capsule())
-                    }
-                    .disabled(viewModel.selectedToken == nil)
+                .submitLabel(.search)
+                .padding(16)
+            
+            Spacer()
+            
+            HStack(spacing: 20) {
+                Button {
+                    dismiss()
+                } label: {
+                    Text("Cancel")
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.white)
+                        .font(.system(size: 17, weight: .semibold))
+                        .padding(.vertical, 11)
+                        .background(Color.clear)
+                        .overlay(
+                            Capsule(style: .circular)
+                                .stroke(.white, lineWidth: 2)
+                        )
                 }
-                .padding(.top, 25)
-                .padding(.bottom, 24)
-                .padding(.horizontal, 20)
+                
+                Button {
+                    showingTokenIdView = true
+                } label: {
+                    Text("Look for tokens")
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.black)
+                        .font(.system(size: 17, weight: .semibold))
+                        .padding(.vertical, 11)
+                        .background(viewModel.tokens.isEmpty ? .white.opacity(0.7) : .white)
+                        .clipShape(Capsule())
+                }
+                .disabled(viewModel.tokens.isEmpty)
             }
-            .navigationTitle("nft.import.title")
+            .padding(.top, 25)
+            .padding(.bottom, 24)
+            .padding(.horizontal, 20)
+        }
+    }
+    
+    func searchTokenByTokenIDView() -> some View {
+        VStack {
+            List(viewModel.tokens, id: \.tokenId) { token in
+                Button {
+                    viewModel.selectedToken = token
+                } label: {
+                    TokenView(token: token, isSelected: viewModel.selectedToken == token)
+                }
+                .onAppear {
+                    if token == viewModel.tokens.last {
+                        viewModel.loadMore()
+                    }
+                }
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.plain)
+            .searchable(text: $searchTokenIdText)
+            .onSubmit(of: .search) {
+                Task {
+                    await viewModel.search(tokenId: searchTokenIdText)
+                }
+            }
+            .padding(.top, 16)
+            
+            HStack(spacing: 20) {
+                Button {
+                    showingTokenIdView = false
+                } label: {
+                    Text("Back")
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.white)
+                        .font(.system(size: 17, weight: .semibold))
+                        .padding(.vertical, 11)
+                        .background(Color.clear)
+                        .overlay(
+                            Capsule(style: .circular)
+                                .stroke(.white, lineWidth: 2)
+                        )
+                }
+                
+                Button {
+                    viewModel.saveToken(viewModel.selectedToken)
+                    dismiss()
+                } label: {
+                    Text("Import")
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.black)
+                        .font(.system(size: 17, weight: .semibold))
+                        .padding(.vertical, 11)
+                        .background(viewModel.selectedToken == nil ? .white.opacity(0.7) : .white)
+                        .clipShape(Capsule())
+                }
+                .disabled(viewModel.selectedToken == nil)
+            }
+            .padding(.top, 25)
+            .padding(.bottom, 24)
+            .padding(.horizontal, 20)
         }
     }
 }
---
+++ ConcordiumWallet/Features/CIS2TokenSupport/ImportToken/ImportTokenViewModel.swift
@@ -0,0 +1,113 @@
+//
+//  ImportTokenViewModel.swift
+//  CryptoX
+//
+//  Created by Zhanna Komar on 19.08.2024.
+//  Copyright © 2024 pioneeringtechventures. All rights reserved.
+//
+
+import SwiftUI
+
+@MainActor
+final class ImportTokenViewModel: ObservableObject {
+    @Published var tokens: [CIS2Token] = []
+    @Published var selectedToken: CIS2Token?
+    @Published var error: ImportTokenError?
+    @Published var isLoading: Bool = false
+    @Published var hasMore: Bool = true
+    @Published var currentPage = 1
+    
+    private let storageManager: StorageManagerProtocol
+    private let address: String
+    private var allContractTokens = [String]()
+    private let batchSize = 20
+    private var contractIndex: Int?
+    
+    private let cis2Service: CIS2Service
+    
+    init(storageManager: StorageManagerProtocol, networkManager: NetworkManagerProtocol, address: String) {
+        self.storageManager = storageManager
+        self.address = address
+        self.cis2Service = CIS2Service(networkManager: networkManager, storageManager: storageManager)
+        
+        logger.debugLog("savedTokens: -- \(self.storageManager.getAccountSavedCIS2Tokens(address))")
+    }
+    
+    func search(name: String) async {
+        do {
+            guard let index = Int(name) else { return }
+            allContractTokens = try await cis2Service.fetchTokens(contractIndex: name).tokens.map(\.token)
+            contractIndex = index
+            loadMore()
+        } catch {
+            logger.errorLog(error.localizedDescription)
+        }
+    }
+    
+    func search(tokenId: String) async {
+        do {
+            guard let contractIndex else { return }
+            tokens = try await cis2Service.fetchAllTokensData(contractIndex: contractIndex, tokenIds: tokenId)
+        } catch {
+            logger.errorLog(error.localizedDescription)
+        }
+    }
+    
+    func saveToken(_ token: CIS2Token?) {
+        guard let token = token else { return }
+        guard !storageManager.getAccountSavedCIS2Tokens(address).contains(token) else { return }
+        
+        do {
+            try storageManager.storeCIS2Token(token: token, address: address)
+        } catch {
+            logger.errorLog(error.localizedDescription)
+        }
+    }
+    
+    func loadMore() {
+        guard !isLoading, hasMore, let contractIndex else { return }
+        
+        isLoading = true
+        
+        Task {
+            do {
+                let ids = allContractTokens.dropFirst((currentPage - 1) * batchSize).prefix(batchSize)
+                
+                guard !ids.isEmpty else {
+                    return await MainActor.run {
+                        hasMore = false
+                        isLoading = false
+                    }
+                }
+                
+                let fetchedTokens = try await self.cis2Service.fetchAllTokensData(contractIndex: contractIndex, tokenIds: ids.joined(separator: ","))
+                
+                await MainActor.run {
+                    
+                    if currentPage == 1 {
+                        tokens = fetchedTokens
+                    } else {
+                        tokens += fetchedTokens
+                    }
+                    hasMore = tokens.count < allContractTokens.count
+                    currentPage += 1
+                    isLoading = false
+                }
+            } catch {
+                await MainActor.run {
+                    isLoading = false
+                }
+            }
+        }
+    }
+    
+    func initialSearchState() {
+        hasMore = true
+        isLoading = false
+        allContractTokens.removeAll()
+        tokens.removeAll()
+        currentPage = 1
+        selectedToken = nil
+        error = nil
+    }
+}
