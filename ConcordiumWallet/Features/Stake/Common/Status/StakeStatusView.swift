90237a22dd92f07b8c8ca340bb7c009eab3d841f
ZhannaKom <zk@concordium.com>
2024-10-04 14:32:53 +0300

    add cooldown cards to the reward setup screen

--- ConcordiumWallet/Features/Stake/Common/Status/StakeStatusView.swift
+++ ConcordiumWallet/Features/Stake/Common/Status/StakeStatusView.swift
@@ -108,47 +108,10 @@ extension StakeStatusView {
         Group {
             if !viewModel.accountCooldowns.isEmpty {
                 ForEach(viewModel.accountCooldowns) { cooldown in
-                    cooldownItemView(cooldown: cooldown)
+                    CooldownCardView(cooldown: cooldown)
                 }
             }
         }
-    }
-    
-    private func cooldownItemView(cooldown: AccountCooldown) -> some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Text("Inactive Stake")
-                .font(.satoshi(size: 14, weight: .medium))
-                .foregroundColor(.white)
-            
-            Text("You don’t receive rewards from this part of stake now, this amount will be at disposal after cooldown period.")
-                .font(.satoshi(size: 12, weight: .regular))
-                .foregroundColor(.gray)
-                .fixedSize(horizontal: false, vertical: true)
-            
-            HStack {
-                Text(GTU(intValue: Int(cooldown.amount))?.displayValue() ?? "")
-                    .font(.satoshi(size: 24, weight: .bold))
-                    .foregroundColor(.white)
-                Spacer()
-            }
-            
-            HStack {
-                Text("Cooldown time:")
-                    .font(.satoshi(size: 14, weight: .medium))
-                    .foregroundColor(.gray)
-                Spacer()
-                Text("\(calculateCooldownTime(from: cooldown.timestamp))")
-                    .foregroundColor(.white)
-                    .font(.satoshi(size: 14, weight: .bold))
-                Text("days left")
-                    .font(.satoshi(size: 14, weight: .medium))
-                    .foregroundColor(.gray)
-            }
-        }
-        .padding()
-        .background(Color(.blackSecondary))
-        .cornerRadius(16)
-        .padding(.top, 10)
     }
     
     // Action Buttons View
@@ -201,12 +164,5 @@ extension StakeStatusView {
     func stopUpdateTimer() {
         updateTimer?.invalidate()
         updateTimer = nil
-    }
-    
-    func calculateCooldownTime(from timestamp: Int) -> Int {
-        let millisecondsInADay: UInt64 = 1000 * 60 * 60 * 24
-        let endDate = Date(timeIntervalSince1970: TimeInterval(timestamp) / 1000.0)
-        let daysToEndCooldown = Int((endDate.millisecondsSince1970 - Date.now.millisecondsSince1970) / millisecondsInADay)
-        return daysToEndCooldown == 0 ? 1 : daysToEndCooldown
     }
 }
