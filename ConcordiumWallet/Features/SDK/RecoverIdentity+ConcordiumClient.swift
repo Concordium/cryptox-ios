fa0630f60d297cc6a7ec6016804a409a372d9237
Max Rachytskyy <makr@concordium.com>
2025-03-14 11:46:30 +0200

    cleanup

---
+++ ConcordiumWallet/Features/SDK/RecoverIdentity+ConcordiumClient.swift
@@ -0,0 +1,13 @@
+//
+//  RecoverIdentity+ConcordiumClient.swift
+//  CryptoX
+//
+//  Created by Max on 14.03.2025.
+//  Copyright © 2025 pioneeringtechventures. All rights reserved.
+//
+
+import Foundation
+
+extension ConcordiumClient {
+    
+}
