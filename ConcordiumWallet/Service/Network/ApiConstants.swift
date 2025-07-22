b99408f47789572123acd43dcfdaf2d55d852eb3
ZhannaKom <zk@concordium.com>
2025-07-22 20:15:32 +0300

    Update ApiConstants.swift

--- ConcordiumWallet/Service/Network/ApiConstants.swift
+++ ConcordiumWallet/Service/Network/ApiConstants.swift
@@ -41,7 +41,6 @@ struct ApiConstants {
     static let accEncryptionKey =       proxyUrl.appendingPathComponent("v0/accEncryptionKey")
     static let submitTransfer =         proxyUrl.appendingPathComponent("v0/submitTransfer")
     static let transferCost =           proxyUrl.appendingPathComponent("v0/transactionCost")
-    static let accountBalance =         proxyUrl.appendingPathComponent("v1/accBalance")
     static let accountBalance =         proxyUrl.appendingPathComponent("v2/accBalance")
     static let accountTransactions =    proxyUrl.appendingPathComponent("v2/accTransactions")
     static let gtuDrop =                proxyUrl.appendingPathComponent("v0/testnetGTUDrop")
